dd103d24c2233b6153ae38c7d7198b8c6aaa2976
paolo berlanda <97700927+pberlanda@users.noreply.github.com>
2022-05-13 12:07:20 +0200

    test Git in ws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoApp';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'magiare', done: true},
+    { description: 'bere', done: true},
+    { description: 'lavorare', done: true},
+    { description: 'dormire', done: true},
+  ]
+
+  get items()  {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item: any) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+  removeAll() {
+    this.allItems = [];
+  }
+
 }
